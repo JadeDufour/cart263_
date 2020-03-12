a303d117b6bab12e91beeefaf456fbd5062027fd
JadeDufour <jade1dufour@gmail.com>
2020-03-12 16:44:42 -0400

    Ex4: added two more descriptions
    -Celebrities
    -Encouraging words

--- assignment4/js/script.js
+++ assignment4/js/script.js
@@ -2,11 +2,8 @@
 
 /********************************************************************
 
-Title of Project
-Author Name
-
-This is a template. Fill in the title, author, and this description
-to match your project! Write JavaScript to do amazing things below!
+Assingment 4
+Jade Dufour
 
 *********************************************************************/
 
@@ -18,37 +15,50 @@ function setup() {
     .done(dataLoaded) // Call dataLoaded on successful load
     .fail(dataNotLoaded); // Call dataNotLoaded on failed load
 
-
-  $.getJSON("assets/data/data.json")
-    .done(dataLoaded) // Call dataLoaded on successful load
-    .fail(dataNotLoaded); // Call dataNotLoaded on failed load
 }
 
 
 function dataLoaded(data) {
   data = data;
   console.log(data);
-
+// Get the random element from the condiment array
   let randomCondiment = getRandomElement(data.condiments);
   console.log(randomCondiment);
-
+// make sure the verb is correct
   let verb = "is";
   if(randomCondiment.charAt(randomCondiment.length - 1) === "s"){
     verb= "are";
 
   }
   console.log(verb);
-
+// Get the random element from the cats array
   let randomCat = getRandomElement(data.cats);
   console.log(randomCat);
-
+// Get the random element from the rooms array
   let randomRoom = getRandomElement(data.rooms);
   console.log(randomRoom);
 
-  let randomDescription = `${randomCondiment} ${verb} like a ${randomCat} in a ${randomRoom}.`;
+  // Get the random element from the encouraging words array
+    let encouragingWords = getRandomElement(data.encouraging_words);
+    console.log(encouragingWords);
+
+    // Get the random element from the encouraging words array
+      let celebrityName = getRandomElement(data.celebrities);
+      console.log(celebrityName);
+
+
+// print the whole description
+  let randomDescription = `Just like ${celebrityName} used to say: "${randomCondiment} ${verb} like a ${randomCat} in a ${randomRoom} ." How ${encouragingWords}!`;
   console.log(randomDescription);
+
+
+
   // let firstShadowMeaning = tarot.tarot_interpretations[4].meanings.shadow[4];
   // console.log(firstShadowMeaning);
+
+
+
+  // dynamically change the css properties
   $("body").append(`<p>${randomDescription}</p>`);
 }
 
