cda11467e4164ac51c6f3b3a5c1f81b15693f860
JadeDufour <jade1dufour@gmail.com>
2020-03-27 17:13:38 -0400

    P3: keep on adding things for the dialogue
    the research is taking a while

--- projet3/js/script.js
+++ projet3/js/script.js
@@ -39,18 +39,28 @@ $clickButton.on('click', beginning)
 function addButton(label , seq) {
   let button = $('<div class="questions"></div>');
   button.text(label);
-
   button.button();
   button.on('click', function() {
     //I used switch states to switch between the questions in the dialogue
     switch (seq) {
       case 1:
-          movingOn();
+          KathyIntro();
           break;
 
       case 2:
-          notMovingOn(seq);
+          explainDID(seq);
           break;
+
+      case 3:
+          howDID();
+          break;
+
+      case 4:
+          meetingTime();
+          break;
+
+
+
 
         }
       });
@@ -71,4 +81,28 @@ function addButton(label , seq) {
         addButton("DID?", 2);
         // keep count of the current alter
         currentAlter = 0;
+      }
+
+
+      function KathyIntro() {
+
+      $('.questions').remove();
+      $('p').empty();
+      text = "Well, my name is Kathy, I consider myself part of a DID system of which I am the host so I use the body's legal name. I'm a 26 years old female and I consider myself as pansexual. I love music and outdoor activities!";
+      $('p').append(text);
+      addButton("Do you consider yourself as the 'original'?", 3);
+      addButton("Do you want me to put on some music?", 4)
+      // keep count of the current alter
+      currentAlter = 0;
+      }
+
+      function original() {
+
+      $('.questions').remove();
+      $('p').empty();
+      text = "No, thats just means that I’m out the most often. So I’m in control of the body the most often. I deal with daily life. I make sure we stay safe and healthy";
+      $('p').append(text);
+      addButton("Can you explain what exactly is DID.", 5);
+      // keep count of the current alter
+      currentAlter = 0;
       }
