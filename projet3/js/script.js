2822cd40f182cf968a9f34bff5847c683a6b26c6
JadeDufour <jade1dufour@gmail.com>
2020-03-27 16:32:22 -0400

    P3 : started adding the dialogue

--- projet3/js/script.js
+++ projet3/js/script.js
@@ -2,7 +2,7 @@
 
 /********************************************************************
 
-Title of Project
+Alters
 Jade Dufour
 
 This is a template. Fill in the title, author, and this description
@@ -17,16 +17,58 @@ $(document).ready(setup);
 
 
 let $html = $('html');
+let text;
+let $clickButton;
 let windowHeight;
 let windowWidth;
-
+let currentAlter;
 function setup() {
 
 
-  particlesJS.load('particles-js', 'assets/data/host.json', function() {});
+// Generate buttons to switch between texts
+$clickButton = $("#click");
+$clickButton.on('click', beginning)
 
+// I changed the cursor because the arrow was a little boring
   $('body').css({
     cursor: 'url(assets/images/hand.png), auto'
   });
 
 }
+
+function addButton(label , seq) {
+  let button = $('<div class="questions"></div>');
+  button.text(label);
+
+  button.button();
+  button.on('click', function() {
+    //I used switch states to switch between the questions in the dialogue
+    switch (seq) {
+      case 1:
+          movingOn();
+          break;
+
+      case 2:
+          notMovingOn(seq);
+          break;
+
+        }
+      });
+       $('.dialogue').append(button);
+     }
+
+     function beginning() {
+       // The particles act as a visual reprentation of each alter but the main
+       // interaction will be a dialogue with the alters
+         particlesJS.load('particles-js', 'assets/data/host.json', function() {});
+
+        $('span').hide();
+        $('.questions').remove();
+        $('p').empty();
+        text = "Hi! My name is Kathy. I'm so glad you invited us today, we can't wait to let the world know about DID! ";
+        $('p').append(text)
+        addButton("It's nice to meet you Kathy. First off all, let me know more about you.", 1);
+        addButton("DID?", 2);
+        // keep count of the current alter
+        currentAlter = 0;
+      }
